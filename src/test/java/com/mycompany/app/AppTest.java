6c42f4e15dc11a430128c6dfda4975778115521e
akash64574 <59645011+akash64574@users.noreply.github.com>
2021-10-29 19:57:08 +0530

    Add files via upload

---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,25 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        
+		//comment the above 2 lines and uncomment below 2 lines to use Chrome
+		//System.setProperty("webdriver.chrome.driver","G:\\chromedriver.exe");
+	
+
+       assertTrue( true );
+    }
+}
